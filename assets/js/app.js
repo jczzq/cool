8ab83113ce628244b283359ba475fbd33735f99e
jczzq <1195262383@qq.com>
2018-07-14 10:34:39 +0800

    主站备案信息

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+new Vue({
+    el: '#app',
+    data: {
+        PAGE_LIST: [
+            {
+                name: '粒子按钮',
+                url: './pages/particle/index.html',
+                cover: 'particle.png'
+            },
+            {
+                name: '富文本编辑器',
+                url: './pages/edit/index.html',
+                cover: 'rich-edit.png'
+            },
+            // {
+            //     name: '菜单布局',
+            //     url: './pages/menu/index.html',
+            //     cover: 'rich-edit.png'
+            // },
+            {
+                name: '3D云在云端',
+                url: './pages/3d-cloud/index.html',
+                cover: '3d-cloud.png'
+            },
+            {
+                name: '海报画廊',
+                url: './pages/poster-gallery/index.html',
+                cover: 'poster-gallery.png'
+            },
+            {
+                name: '拼图游戏',
+                url: './pages/puzzle/index.html',
+                cover: 'puzzle.png'
+            },
+            {
+                name: '气泡',
+                url: './pages/pop-wave/index.html',
+                cover: 'pop-wave.png'
+            }
+        ]
+    }
+});
